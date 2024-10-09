934198c8f0eb3eafb1f5ae7856d7a1010318ea1e
Seung Jun Lee <mps86@naver.com>
2024-10-09 16:43:10 +0900

    first commit

---
+++ LessonlinkApplication.java
@@ -0,0 +1,13 @@
+package com.lessonlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonlinkApplication.class, args);
+	}
+
+}
