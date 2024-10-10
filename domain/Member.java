1ce706d09996db2c2d691ff735377b9f23e66457
Seung Jun Lee <mps86@naver.com>
2024-10-10 22:07:44 +0900

    fix: add @Setter annotation to Member Domain to resolve property access issue

--- domain/Member.java
+++ domain/Member.java
@@ -2,12 +2,13 @@ package com.lessonlink.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter
+@Getter @Setter
 public class Member {
 
     @Id
