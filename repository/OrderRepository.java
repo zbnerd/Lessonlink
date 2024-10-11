0c300d098e2e52e9a0593850db210a9c13ead085
Seung Jun Lee <mps86@naver.com>
2024-10-11 13:13:19 +0900

    feat : create orderService biz logic

---
+++ repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package com.lessonlink.repository;
+
+import com.lessonlink.domain.Order;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+// public List<Order> findAll(OrderSearch orderSearch) { ... }
+}
