422e78f554395b304d1c8ba81d5c78e2074ebbc3
Seung Jun Lee <mps86@naver.com>
2024-10-11 16:58:39 +0900

    feat: add MemberController and home page handling
    - Implement HomeController to handle root ("/") requests
    - Add MemberController to manage member creation and listing
    - Create MemberForm with validation for member input

---
+++ web/MemberController.java
@@ -0,0 +1,51 @@
+package com.lessonlink.web;
+
+import com.lessonlink.domain.Address;
+import com.lessonlink.domain.Member;
+import com.lessonlink.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());  // 수정된 부분
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(@Valid MemberForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setAddress(address);
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    //추가
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
