1a3bf06d3f67db19e96ec45567b097ef93eaf23b
Seung Jun Lee <mps86@naver.com>
2024-10-15 00:14:00 +0900

    style: remove unnecessary import statements from MemberRepository

--- repository/MemberRepository.java
+++ repository/MemberRepository.java
@@ -2,7 +2,6 @@ package com.lessonlink.repository;
 
 import com.lessonlink.domain.member.Member;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
