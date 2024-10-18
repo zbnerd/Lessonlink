9d489c61cef52b42d9c073f8ffdf78c58084c6ea
Seung Jun Lee <mps86@naver.com>
2024-10-18 18:26:26 +0900

    feat: 주문 검색 기능 추가
    - OrderRepository에 주문 검색 기능 추가 (findAllByString 메서드)
    - OrderSearch 객체를 통해 주문 상태와 회원 이름으로 검색 가능하도록 구현
    - JPQL 동적 쿼리 생성으로 주문 상태 및 회원 이름 조건에 따라 검색 필터링
    - 최대 1000건까지 검색 결과 제한 설정
    - 기존의 기본 주문 조회 방식 외에 다양한 검색 옵션 제공을 통해 유연한 주문 관리 가능

---
+++ domain/order/OrderSearch.java
@@ -0,0 +1,11 @@
+package com.lessonlink.domain.order;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus;//주문 상태[ORDER, CANCEL]
+}
