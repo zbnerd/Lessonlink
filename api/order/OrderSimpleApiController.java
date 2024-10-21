d48082a43ae453cfa179574a683d69bf25294fa2
Seung Jun Lee <mps86@naver.com>
2024-10-21 16:51:23 +0900

    feat: 주문 조회 API DTO 변환 기능 추가
    - beta-V1: 엔티티를 직접 노출하는 간단한 주문 조회 API 추가
      - Hibernate5Module을 통한 LAZY 처리
      - 양방향 관계 문제 해결을 위한 @JsonIgnore 적용
    - beta-V2: 엔티티를 DTO로 변환하여 조회하는 API 추가
      - DTO 변환을 통해 필요한 데이터만 반환하도록 개선
      - 단, 지연 로딩으로 인해 N+1 문제 발생 가능

--- api/order/OrderSimpleApiController.java
+++ api/order/OrderSimpleApiController.java
@@ -1,12 +1,16 @@
 package com.lessonlink.api.order;
 
+import com.lessonlink.domain.common.embedded.Address;
 import com.lessonlink.domain.order.Order;
 import com.lessonlink.domain.order.enums.OrderSearch;
+import com.lessonlink.domain.order.enums.OrderStatus;
 import com.lessonlink.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -14,11 +18,11 @@ import java.util.List;
 public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
     /**
-     * V1. 엔티티 직접 노출
+     * beta-V1. 엔티티 직접 노출
      * - Hibernate5Module 모듈 등록, LAZY=null 처리
      * - 양방향 관계 문제 발생 -> @JsonIgnore
      */
-    @GetMapping("/api/v0/simple-orders")
+    @GetMapping("/api/beta-v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         for (Order order : all) {
@@ -26,5 +30,36 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); //Lazy 강제 초기화
         }
         return all;
+    }
+
+    /**
+     * beta-V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     * -
+     */
+
+    @GetMapping("/api/beta-v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .toList();
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
