b43256fa70efd10b7b4e853ff2e5b345d500054b
Seung Jun Lee <mps86@naver.com>
2024-11-15 16:01:32 +0900

    feat: 대량 회원 데이터 삽입을 위한 MemberRepositoryBulk 추가 및 인터페이스 확장
    - JdbcTemplate을 활용하여 `MemberRepositoryBulk`에 `bulkInsertMembers` 메서드 구현
    - 대량 회원 데이터를 효율적으로 삽입하기 위해 `batchUpdate` 적용
    - `MemberRepositoryCustom` 인터페이스를 도입하여 `MemberRepository`와 `MemberRepositoryBulk`의 의존성 주입 가능하도록 확장
    - 기존 `MemberRepository`에 `MemberRepositoryCustom` 인터페이스 추가하여 확장성 개선

--- repository/MemberRepository.java
+++ repository/MemberRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, String>, MemberRepositoryCustom {
 
     @Query("select m from Member m where m.memberId = :memberId")
     Optional<Member> findByMemberId(@Param("memberId") String memberId);
